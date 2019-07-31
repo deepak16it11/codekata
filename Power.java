f3c7ead91e1ecf68a50911c09014635b72224079
deepak16it11 <47378404+deepak16it11@users.noreply.github.com>
2019-07-31 12:14:35 +0530

    Create Power.java

---
+++ Power.java
@@ -0,0 +1,9 @@
+public class Power
+{
+	public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        int n=input.nextInt();
+        int m=input.nextInt();
+        System.out.print((int)Math.pow(n,m));
+	}
+}
