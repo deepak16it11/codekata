fb134fa80cffd6a77dee8cbb2451f41a2a1a0bc3
deepak16it11 <47378404+deepak16it11@users.noreply.github.com>
2019-08-02 16:21:50 +0530

    Create Prime.java

---
+++ Prime.java
@@ -0,0 +1,20 @@
+    import java.util.*;
+    public class Prime{  
+     public static void main(String args[]){  
+      Scanner deepak = new Scanner(System.in);
+      int n= deepak.nextInt();  
+       int flag=0;
+       for(int i=1;i<=n;i++) 
+       {
+        
+           if(n%i==0)
+           {
+               flag++;
+           }
+       }
+      if(flag==2)    
+       System.out.println("yes");    
+      else    
+       System.out.println("no");    
+    }  
+    }  
